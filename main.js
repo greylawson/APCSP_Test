03fe8cb32eff91bb1f5ed5e73648ed86d000dc25
Grey Lawson <greylawson12@gmail.com>
2018-12-06 17:52:31 -0600

    Fixed issue where apples did not appear

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
 var pg;
+var feed = [];
+var amount = prompt("How many apples do you want to feed the animals?");
+var numFood = amount;
 
 function setup() {
     createCanvas(740, 480);
     pg = new Pig();
-    var foodNum = prompt("How many apples do you want to feed the animals?");
+    
+    // initializing a set amount number of food
+    for(var i = 0; i < numFood; i++) {
+        feed.push(new Food(random(width), random(height)));
+    }
 }
 
 function draw() {
     background(0,250,0);
     pg.display();
+    
+    // display all the food
+    for(var i = 0; i < feed.length; i++) {
+        feed[i].display();
+    }
 }
 
+function mousePressed() {
+    pg.eat();
+}
+
+function Food(x, y) {
+    // keyword this
+    // makes variables public on the object
+    
+    // public instance variables
+    this.x = x;
+    this.y = y;
+    this.color = color(255, 0, 0);
+    this.foodSize = 50;
+    
+    this.display = function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.foodSize, this.foodSize);
+    }
+}
 
 
 function Pig() {
     var x = mouseX;
     var y = mouseY;
+    var diameter = 200;
+
+    this.getDistance = function(other) {
+        var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+        return dist;
+    }
+    
+    this.eat = function() {
+        for(var i = 0; i < feed.length ; i++) {
+            var food = feed[i];
+            var d = this.getDistance(food);
+            var r1 = food.foodSize / 2;
+            var r2 = diameter / 2;
+            if(r1 + r2 > d) {
+                feed.splice(i, 1);
+            }
+        }
+    }
     
     this.display = function() {
         var x = mouseX;
