9a29aaa01e1e8fab1263826b4c087b3de99ae4d0
Grey Lawson <greylawson12@gmail.com>
2018-12-07 11:43:19 -0600

    Tried to make the pig eat

--- main.js
+++ main.js
@@ -53,8 +53,9 @@ function Pig() {
     var diameter = 200;
 
     this.getDistance = function(other) {
-        var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
-        return dist;
+        var d = dist(x, other.x, y, other.y);
+        //var dist = Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+        return d;
     }
     
     this.eat = function() {
@@ -63,6 +64,9 @@ function Pig() {
             var d = this.getDistance(food);
             var r1 = food.foodSize / 2;
             var r2 = diameter / 2;
+            console.log(r1);
+            console.log(r2);
+            console.log(d);
             if(r1 + r2 > d) {
                 feed.splice(i, 1);
             }
@@ -73,95 +77,49 @@ function Pig() {
         var x = mouseX;
         var y = mouseY;
     
-    //draws a puppy
-    //face
-    noStroke();
-    fill('#52C5DC');
-    ellipse(x, y, 200, 200);
-    
-    //nose
-    fill('#EFB8D2');
-    ellipse(x, y+30, 64, 64);
-    
-    //eyeLeft
-    fill('#FFFFFF');
-    ellipse(x+42, y-26, 64, 64);
-    
-    //pupilLeft
-    fill('#7FC35E');
-    ellipse(x+42, y-26, 40, 40);
-    
-    //eyeRight
-    fill('#FFFFFF');
-    ellipse(x-42, y-26, 64, 64);
-    
-    //pupilRight
-    fill('#7FC35E');
-    ellipse(x-42, y-26, 40, 40);
-    
-    //earLeft
-    fill('#52C5DC');
-    push();
-    translate(x-100, y-80);
-    rotate(Math.PI/4);
-    ellipse(0, 0, 52, 92);
-    pop();
-    
-    //earRight
-    fill('#52C5DC');
-    push();
-    translate(x+100, y-80);
-    rotate(-Math.PI/4);
-    ellipse(0, 0, 52, 92);
-    pop();
-    
-    //tongue
-    fill('#EE3E36');
-    arc(x-20, y+80, 40, 70, 0, PI+QUARTER_PI, CHORD);
-    
     //draws a pig
     //face
     noStroke();
     fill('#FF99FF');
-    ellipse(x+380, y, 200, 200);
+    ellipse(x, y, 200, 200);
     
     //earLeft
     noStroke();
     fill('#FF99FF');
-    triangle(x+290, y-50, x+324, y-90, x+280, y-115);
+    triangle(x-80, y-68, x-20, y-105, x-70, y-120);
     
     //earRight
     noStroke();
     fill('#FF99FF');
-    triangle(x+420, y-95, x+465, y-120, x+460, y-67);
+    triangle(x+80, y-68, x+20, y-105, x+70, y-120);
     
     //eyeLeft
     fill('#FFFFFF');
-    ellipse((x+380)-42, y-26, 54, 54);
+    ellipse(x-42, y-26, 54, 54);
     
     //pupilLeft
     fill('#0000FF');
-    ellipse((x+380)-42, y-26, 30, 30);
+    ellipse(x-42, y-26, 30, 30);
     
     //eyeRight
     fill('#FFFFFF');
-    ellipse((x+380)+42, y-26, 54, 54);
+    ellipse(x+42, y-26, 54, 54);
     
     //pupilRight
     fill('#0000FF');
-    ellipse((x+380)+42, y-26, 30, 30);
+    ellipse(x+42, y-26, 30, 30);
     
     //nose
     fill('#FF3399');
-    rect((x+380)-25, y+10, 50, 50);
+    rect(x-25, y+10, 50, 50);
     
     //nostrilLeft
     fill('#000000');
-    ellipse((x+380)-12, y+20, 13, 13);
+    ellipse(x-12, y+20, 13, 13);
     
     //nostrilRight
     fill('#000000');
-    ellipse((x+380)+12, y+20, 13, 13);
+    ellipse(x+12, y+20, 13, 13);
     }
 }
 
